e98d8b8d39d5a3811f26d73efc3334202b1705e0
Jose Lopez <jmlc11@outlook.com>
2013-11-25 00:11:33 -0400

    Para que sea void el generador de QR

--- Dogout Reporting Application/Dogout Reporting Application/GeneradorQR.cs
+++ Dogout Reporting Application/Dogout Reporting Application/GeneradorQR.cs
@@ -23,14 +23,13 @@ namespace Dogout_Reporting_Application
             pictureBox1.Image = GeneradorQR.GenerateQR(textBox1.Text);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
-        public static Bitmap GenerateQR(string text)
+        public static void GenerateQR(string text)
         {
             IBarcodeWriter writer = new BarcodeWriter { Format = BarcodeFormat.QR_CODE };
             Bitmap barcodeBitmap;
             var result = writer.Write("http://dogout.azurewebsites.net/Ticket/Details/" + text);
             barcodeBitmap = new Bitmap(result, new Size(256, 256));
             barcodeBitmap.Save("qr.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
-            return barcodeBitmap;
         }
     }
 }
