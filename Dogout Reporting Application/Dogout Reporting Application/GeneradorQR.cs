2ba242d79a5b3c195f8fc6eed9f669329dc40170
Jose Lopez <jmlc11@outlook.com>
2013-11-25 00:18:17 -0400

    Sobrecarga de entero.

--- Dogout Reporting Application/Dogout Reporting Application/GeneradorQR.cs
+++ Dogout Reporting Application/Dogout Reporting Application/GeneradorQR.cs
@@ -20,7 +20,9 @@ namespace Dogout_Reporting_Application
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Image = GeneradorQR.GenerateQR(textBox1.Text);
+            GeneradorQR.GenerateQR(textBox1.Text);
+            Bitmap bmp = new Bitmap("qr.bmp");
+            pictureBox1.Image = bmp;
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
         public static void GenerateQR(string text)
@@ -28,6 +30,14 @@ namespace Dogout_Reporting_Application
             IBarcodeWriter writer = new BarcodeWriter { Format = BarcodeFormat.QR_CODE };
             Bitmap barcodeBitmap;
             var result = writer.Write("http://dogout.azurewebsites.net/Ticket/Details/" + text);
+            barcodeBitmap = new Bitmap(result, new Size(256, 256));
+            barcodeBitmap.Save("qr.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+        public static void GenerateQR(int numero)
+        {
+            IBarcodeWriter writer = new BarcodeWriter { Format = BarcodeFormat.QR_CODE };
+            Bitmap barcodeBitmap;
+            var result = writer.Write("http://dogout.azurewebsites.net/Ticket/Details/" + numero.ToString());
             barcodeBitmap = new Bitmap(result, new Size(256, 256));
             barcodeBitmap.Save("qr.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
         }
